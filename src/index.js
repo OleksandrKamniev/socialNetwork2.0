abfb223f2df90c44448338ddf86295f384d10cfe
Oleksandr Kamniev <sashkam75@gmail.com>
2023-04-07 20:07:40 +0300

    Store. State. OOP

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-
+import store from "./components/redux/state";
 import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {rerenderEntireTree} from "./render";
-import state from "./components/redux/state";
+import {BrowserRouter} from "react-router-dom";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+export let rerenderEntireTree=(state)=>{
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App
+                    state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}
+                />
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+}
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
-rerenderEntireTree(state)
-
+rerenderEntireTree(store.getState())
+store.subscribe(rerenderEntireTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
