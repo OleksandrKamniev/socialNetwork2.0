865947769106266fa20bf3c9c7f2b74e856f5c54
Oleksandr Kamniev <sashkam75@gmail.com>
2023-04-24 15:16:05 +0300

    Adding Preloader and refacting mapDispatchToProps in Users

---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,8 @@
+import preloader from "./../../../asserts/loading.gif";
+import React from "react";
+let Preloader=()=>{
+    return <div>
+        <img src={preloader} alt="preloader" />
+    </div>
+}
+export default Preloader;
