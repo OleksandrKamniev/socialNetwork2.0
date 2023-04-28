cc1084c8c2b97697d169dd5a28d4e2bac280d424
Oleksandr Kamniev <sashkam75@gmail.com>
2023-04-28 21:08:36 +0300

    cookie/login/auth-me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import classes from "./Header.module.css";
-const Header = () => {
+import styles from "./Header.module.css";
+import {NavLink} from "react-router-dom";
+const Header = (props) => {
   return (
-    <header className={classes.header}>
+    <header className={styles.header}>
       <img
         src="https://www.logodesignlove.com/wp-content/uploads/2022/11/carebibi-logo-01-1200x1200.jpeg"
         alt="logo"
       />
+        <div className={styles.loginBlock}>
+            {props.isAuth ? props.login :<NavLink to={"/login"}>Login</NavLink>}
+
+        </div>
     </header>
   );
 };
