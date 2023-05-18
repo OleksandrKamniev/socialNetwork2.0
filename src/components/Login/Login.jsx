147dd8f33644224a7e1e784c42c437ac1ff38171
Oleksandr Kamniev <sashkam75@gmail.com>
2023-05-18 20:38:14 +0300

    Adding forms(by formik)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,68 @@
 import React from 'react';
-
-let Login =()=>{
-    return <div>
-        Login
+import { Formik } from 'formik';
+const Login = () => (
+    <div>
+        <h1>Anywhere in your app!</h1>
+        <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+                const errors = {};
+                if (!values.email) {
+                    errors.email = 'Required';
+                } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                    errors.email = 'Invalid email address';
+                }
+                return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                  /* and other goodies */
+              }) => (
+                <form onSubmit={handleSubmit}>
+                    <div>
+                    <input
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                    />
+                    </div>
+                    {errors.email && touched.email && errors.email}
+                <div>
+                    <input
+                        type="password"
+                        name="password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                    />
+                </div>
+                    {errors.password && touched.password && errors.password}
+                <div>
+                    <button type="submit" disabled={isSubmitting}>
+                        Submit
+                    </button>
+                </div>
+                </form>
+            )}
+        </Formik>
     </div>
-}
-
+);
 
 export default Login;
