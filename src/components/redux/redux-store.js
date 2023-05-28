f2478244964dcc13df4d9f818f36d36df150ee64
Oleksandr Kamniev <sashkam75@gmail.com>
2023-05-28 12:33:22 +0300

    Adding Chrome extensions(React Developer Tools and Redux Dev Tools)

--- src/components/redux/redux-store.js
+++ src/components/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux"
+import {applyMiddleware, combineReducers, compose, createStore} from "redux"
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import usersReducer from "./usersReducer";
@@ -11,6 +11,11 @@ let reducers=combineReducers({
     usersPage: usersReducer,
     auth: authReducer,
 })
-let store =createStore(reducers, applyMiddleware(thunkMiddleware));
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+   applyMiddleware(thunkMiddleware)
+));
 
 export default store;
